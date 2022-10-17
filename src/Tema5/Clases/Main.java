fe417b7001a49a5ebaab0e5e2c8ba4944a98d056
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-17 18:46:14 +0200

    polimorfismo

--- src/Tema5/Clases/Main.java
+++ src/Tema5/Clases/Main.java
@@ -1,5 +1,7 @@
 package Tema5.Clases;
 
+import Tema5.Herencia.Camion;
+import Tema5.Herencia.Coche;
 import Tema5.Herencia.Motocicleta;
 
 public class Main {
@@ -14,11 +16,16 @@ public class Main {
         System.out.println(coche1.motor.velMax);
         System.out.println(coche2.motor.velMax);
 
+
+        //HERENCIA
         Motocicleta ducatiMonster = new Motocicleta(true,"Disco",true,false);
         ducatiMonster.marca = "Ducati";
         ducatiMonster.modelo = "Monster";
+        ducatiMonster.velocidad = 0.0;
+        ducatiMonster.acelerar(300);
 
         System.out.println(ducatiMonster.marca);
+        System.out.println(ducatiMonster.velocidad);
 
 
 
@@ -29,6 +36,19 @@ public class Main {
         System.out.println("Velocidad coche2: "+coche2.velocidad +"km/h");
         coche2.acelerar(190);
         System.out.println("Velocidad coche2: "+coche2.velocidad +"km/h");
+
+         //POLIMORFISMO
+        Vehiculo vehiculo;
+        vehiculo = new Motocicleta(true, "Tambor",false,true);
+        vehiculo.acelerar(150);
+
+        vehiculo = new Vehiculo("Renault", "Megane","Audi",200.0,"Azul",true,2009, 0.0,motorGti);
+        vehiculo.acelerar(90);
+
+
+        vehiculo = new Camion();
+        vehiculo.acelerar(20);
+
 
 
         System.out.println("FIN DE PROGRAMA");

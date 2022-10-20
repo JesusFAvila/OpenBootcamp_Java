45117c67951605fe61af465fbcbd95f7c00fee94
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-20 18:29:20 +0200

    tema7

---
+++ src/Tema8/DatosAvanzados.java
@@ -0,0 +1,10 @@
+package Tema8;
+
+public class DatosAvanzados {
+    public static void main(String[] args) {
+        String cadenaTexto = "Esto es una cadena de texto";
+        int calcularTexto = cadenaTexto.length();
+        System.out.println(calcularTexto);
+    }
+
+}

3b3815f4d42052a32f12d10f6e8f83bd6d25059e
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-21 12:08:05 +0200

    Tema8

--- src/Tema8/DatosAvanzados.java
+++ src/Tema8/DatosAvanzados.java
@@ -4,7 +4,12 @@ public class DatosAvanzados {
     public static void main(String[] args) {
         String cadenaTexto = "Esto es una cadena de texto";
         int calcularTexto = cadenaTexto.length();
-        System.out.println(calcularTexto);
+        System.out.println("La longitud de mi cadena de texto es de: " + calcularTexto);
+        String cadenaMayus = cadenaTexto.toUpperCase();
+        String cadenaMinus = cadenaTexto.toLowerCase();
+        System.out.println(cadenaMayus);
+        System.out.println(cadenaMinus);
+
     }
 
 }

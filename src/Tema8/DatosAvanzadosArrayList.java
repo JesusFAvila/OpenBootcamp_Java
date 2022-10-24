dc1fe9bd8c4642184cb21c49bd7b70170a9f8574
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-24 19:40:42 +0200

    Recorrer ArrayList

--- src/Tema8/DatosAvanzadosArrayList.java
+++ src/Tema8/DatosAvanzadosArrayList.java
@@ -6,6 +6,7 @@ public class DatosAvanzadosArrayList {
     public static void main(String[] args) {
         //Array List: listas en formato array
         //La principal diferencia respecto un vector es la capacidad que ocupa, mientras que el vector la dobla, el array list aumenta sun 50%
+        //Los arraylist no están sincronizados, no se pueden modificar de forma concurrente dos arraylist
 
         ArrayList<String> listaString = new ArrayList<String>();
         listaString.add("Element A");
@@ -14,6 +15,23 @@ public class DatosAvanzadosArrayList {
         System.out.println
                 ("Mostrar elementos de lista: " + listaString);
 
+        //Eliminar elementos
+        listaString.remove("Element B");
+        System.out.println
+                ("Mostrar elementos de lista actualizados: " + listaString);
+
+        //Recorrer Arraylist
+        //Forma larga
+        for (int i = 0 ; i < listaString.size() ; i++){
+            System.out.println
+                    ("La posicion del elemento " + listaString.get(i) + " es: " + i);
+        }
+
+        //Forma corta
+        for (String i : listaString){
+            System.out.println
+                    ("La posicion es: " + i);
+        }
 
 
 

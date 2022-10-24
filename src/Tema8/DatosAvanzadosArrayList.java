fff0fa1bf27190a3b7c67ccbb5fdc1200197b044
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-24 19:27:28 +0200

    ArrayList

---
+++ src/Tema8/DatosAvanzadosArrayList.java
@@ -0,0 +1,22 @@
+package Tema8;
+
+import java.util.ArrayList;
+
+public class DatosAvanzadosArrayList {
+    public static void main(String[] args) {
+        //Array List: listas en formato array
+        //La principal diferencia respecto un vector es la capacidad que ocupa, mientras que el vector la dobla, el array list aumenta sun 50%
+
+        ArrayList<String> listaString = new ArrayList<String>();
+        listaString.add("Element A");
+        listaString.add("Element B");
+        listaString.add("Element C");
+        System.out.println
+                ("Mostrar elementos de lista: " + listaString);
+
+
+
+
+
+    }
+}

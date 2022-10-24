81460ec8b306046c3b678b4ce3c583a12eefff1e
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-24 12:57:50 +0200

    Tab

--- src/Tema8/DatosAvanzadosString.java
+++ src/Tema8/DatosAvanzadosString.java
@@ -5,7 +5,8 @@ public class DatosAvanzadosString {
 
         String cadenaTexto = "Esto es una cadena de texto";
         int calcularTexto = cadenaTexto.length();
-        System.out.println("La longitud de mi cadena de texto es de: " + calcularTexto);
+        System.out.println
+                ("La longitud de mi cadena de texto es de: " + calcularTexto);
 
         String cadenaMayus = cadenaTexto.toUpperCase();
         System.out.println(cadenaMayus);
@@ -14,16 +15,20 @@ public class DatosAvanzadosString {
         System.out.println(cadenaMinus);
 
         boolean pruebaFuncionEmpiezaString = cadenaTexto.startsWith("Esto es");
-        System.out.println("la función cadenaTexto empieza por 'Esto es': "+ pruebaFuncionEmpiezaString);
+        System.out.println
+                ("la función cadenaTexto empieza por 'Esto es': "+ pruebaFuncionEmpiezaString);
 
         boolean pruebaFuncionAcabaString = cadenaTexto.endsWith("de texto");
-        System.out.println("la función cadenaTexto acaba por 'de texto': "+ pruebaFuncionAcabaString);
+        System.out.println
+                ("la función cadenaTexto acaba por 'de texto': "+ pruebaFuncionAcabaString);
 
         char letra = cadenaTexto.charAt(3);
-        System.out.println("La letra que ocupa posición 5 de la cadena 'Esto es una cadena de texto' es la letra: " + letra);
+        System.out.println
+                ("La letra que ocupa posición 5 de la cadena 'Esto es una cadena de texto' es la letra: " + letra);
 
         for (int i=0; i<cadenaTexto.length(); i++){
-            System.out.println("utilizamos un for para recorrer un String como si se tratara de un array: " +cadenaTexto.charAt(i));
+            System.out.println
+                    ("utilizamos un for para recorrer un String como si se tratara de un array: " +cadenaTexto.charAt(i));
         }
 
         //Ejercicio: invertir un Strin en Java.
@@ -31,8 +36,10 @@ public class DatosAvanzadosString {
         for (int i = cadenaTexto.length() -1; i >= 0; i--){
             invertirString += cadenaTexto.charAt(i);
         }
-        System.out.println("Cadena de texto original: "+ cadenaTexto);
-        System.out.println("Cadena de texto invertida: "+ invertirString);
+        System.out.println
+                ("Cadena de texto original: "+ cadenaTexto);
+        System.out.println
+                ("Cadena de texto invertida: "+ invertirString);
 
 
 
--- src/Tema8/DatosAvanzadosVectores.java
+++ src/Tema8/DatosAvanzadosVectores.java
@@ -7,7 +7,9 @@ public class DatosAvanzadosVectores {
     public static void main(String[] args) {
         //A diferencia del array no hace falta asignarle un tamaño al principio o al final.
         //Tienen dos propiedades el tamaño (.size) y la capacidad (.capacity)
+        //La capacidad del vector inicialmente es siempre 10 (Capacidad real del array subyacente), pero aumentará de 10 en 10 progresivamente.
 
+        //VECTOR INT
         Vector <Integer> vectorInt = new Vector<Integer>();
         vectorInt.add(12);
         vectorInt.add(18);
@@ -16,30 +18,37 @@ public class DatosAvanzadosVectores {
         vectorInt.add(80);
         vectorInt.add(99);
         vectorInt.add(30);
-        System.out.println("Datos del vectorInt: " + vectorInt);
+        System.out.println
+                ("Datos del vectorInt: " + vectorInt);
 
         //metodo de vector para eliminar indices
         vectorInt.remove(2);
-        System.out.println("Datos del vectorInt: " + vectorInt);
+        System.out.println
+                ("Datos del vectorInt: " + vectorInt);
 
         //Métodos para comprabar la capacidad y el tamaño del vector
-        System.out.println("Comprobamos el capacidad del vectorInt: " + vectorInt.capacity() + " y de tamaño: " + vectorInt.size());
+        System.out.println
+                ("Comprobamos el capacidad del vectorInt: " + vectorInt.capacity() + " y de tamaño: " + vectorInt.size());
 
+        //VECTOR STRING
         Vector <String>vectorString =  new Vector<String>();
         vectorString.add("Hola");
         vectorString.add("esto");
         vectorString.add("es");
         vectorString.add("un");
         vectorString.add("vector tipo String");
-        System.out.println("Datos del vectorString: " + vectorString);
+        System.out.println
+                ("Datos del vectorString: " + vectorString);
 
         vectorString.remove(4);
-        System.out.println("Datos del vectorString: " + vectorString);
+        System.out.println
+                ("Datos del vectorString: " + vectorString);
         vectorString.remove("Hola");
-        System.out.println("Datos del vectorString: " + vectorString);
+        System.out.println
+                ("Datos del vectorString: " + vectorString);
+        System.out.println
+                ("Comprobamos el capacidad del vectorString: " + vectorString.capacity() + " y de tamaño: " + vectorString.size());
 
-        System.out.println("Comprobamos la tamaño del vectorString: " + vectorString.size());
-        System.out.println("Comprobamos el capacidad del vectorString: " + vectorString.capacity());
 
 
 

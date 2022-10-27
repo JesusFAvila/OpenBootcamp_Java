f9e3d50231c26a512ab5bb0beb9d09b26724a2fa
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-27 10:36:07 +0200

    LinkedList sout - add

--- src/Tema8/DatosAvanzadosLinkedList.java
+++ src/Tema8/DatosAvanzadosLinkedList.java
@@ -8,9 +8,15 @@ public class DatosAvanzadosLinkedList {
         //DIFERENCIA ARRAYLIST
             //1- Un arraylist utiliza un array (Mucho más rápido a la hora de almacenar y buscar datos) de forma subyace, un linked list no, utiliza una lista doblemente enlazada (Más rápida cuando queremos modificar datos, no almacenar ni buscar).
             //2- El array list deriva de la clase list, la lista enlazada implementa los interface, podemos utilizar la linked-list para utilizar colas.
-
-
         LinkedList<String> listaEnlazada = new LinkedList<String>();
+        listaEnlazada.add("Element A");
+        listaEnlazada.add("Element B");
+        listaEnlazada.add("Element C");
+        System.out.println(listaEnlazada);
+        System.out.println(listaEnlazada.size());
+        System.out.println(listaEnlazada.add("Element D"));
+        System.out.println(listaEnlazada);
+
 
     }
 }

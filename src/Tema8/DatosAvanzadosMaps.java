f17ddd09f9a79880839870fb19360dfd761632ac
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-27 11:25:16 +0200

    HashMap .get .replace

--- src/Tema8/DatosAvanzadosMaps.java
+++ src/Tema8/DatosAvanzadosMaps.java
@@ -8,18 +8,32 @@ public class DatosAvanzadosMaps {
         //Diccionario donde tenemos una clave y un valor
 
         //La forma más primitiva de mapa es el HashMap:
-        //En el ejemplo vemos el tiempo de dato String e Integer: Esto significa que mi mapa es un diccionario cuyas claves van a ser cadenas de texto y los valores asociados a cada una de esas claves van a ser un entero.
+        //En el ejemplo vemos el tiempo de dato String e Integer: Esto significa que mi mapa es un diccionario cuyas -
+            // claves van a ser cadenas de texto y los valores asociados a cada una de esas claves van a ser un entero.
         //Los HashMap tienen una peculiaridad y es que no podemos tener la clave duplicada.
         HashMap<String, Integer> mapPrimitivo = new HashMap<String, Integer>();
-
         //Añadir elemento al HashMap mediante .put
         //Utilizaremos los dos parametros, la clave y el valor
         mapPrimitivo.put("clave1", 120);
         mapPrimitivo.put("clave2", 987);
         mapPrimitivo.put("clave3", 32);
-        mapPrimitivo.put("clave4", 11);
-
         System.out.println(mapPrimitivo);
+
+        //El valor se sobreescribe mediante .put
+        mapPrimitivo.put("clave4", 11);
+        mapPrimitivo.put("clave4", 22);
+        System.out.println(mapPrimitivo);
+
+        //Método para consultar una clave determinada mediante.get
+        System.out.println
+                ("Utilizamos el metodo .get " + mapPrimitivo .get("clave3"));
+
+        //Reemplazar un valor mediante .replace
+        System.out.println(mapPrimitivo .replace("clave2", 1));
+        System.out.println
+                ("Utilizamos el metodo .replace " + mapPrimitivo .get("clave2"));
+
+
 
 
 

7ae3a922d1814966986c8b42fed59245113732e4
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-27 11:03:15 +0200

    DatosAvanzadosMaps HashMap

---
+++ src/Tema8/DatosAvanzadosMaps.java
@@ -0,0 +1,13 @@
+package Tema8;
+
+import java.util.HashMap;
+
+public class DatosAvanzadosMaps {
+    public static void main(String[] args) {
+        //MAPAS / ARRAY ASOCIATIVO
+        //Diccionario donde tenemos una clave y un valor
+        //La forma más primitiva de mapa es el HashMap
+        HashMap<String, Integer> mapPrimitivo = new HashMap<String, Integer>();
+
+    }
+}

c5358dd0b6aeb3a01638cf16a40669c1c683d333
Jesús F.Ávila <jesusfernandezavila91@gmail.com>
2022-10-28 11:17:13 +0200

    HashMap warnings2 TryCach

---
+++ src/Tema8/ErrorRunTime.java
@@ -0,0 +1,34 @@
+package Tema8;
+
+import java.util.Scanner;
+
+public class ErrorRunTime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce dos números: ");
+        int numero1 = scanner.nextInt();
+        int numero2 = scanner.nextInt();
+
+        //Cuando dividimos en numero1 4 y numero2 0 se produce un error de runtime
+        //Para evitar que el user itroduzca en el numero2 el valor 0 utilizaremos try:
+        //En el carch especidicamos la excepción que queremos capturar mediante exception e .
+
+        //int resultado = numero1 / numero2;
+        //System.out.println("El resultado es: " + resultado);
+
+        try {
+            int resultado = numero1 / numero2;
+            System.out.println("El resultado es: " + resultado);
+        }catch (ArithmeticException e){
+            System.out.println("Boom!, excepción es: " + e.getClass());
+        }catch (Exception e){
+            //Exception e calcula todas las excepciones que hay en Java , para calcular 1 especifica tenemos que decir-
+            //la excepcion especifica que quiero controlar, en este caso la aritmética
+            System.out.println("La excepción no es aritmética");
+        }finally {
+            System.out.println("Fin del programa error");
+        }
+
+    }
+
+}

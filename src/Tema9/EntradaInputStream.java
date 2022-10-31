902f5cdd637e6204db19290e8c3d97d68ff2e233
Jesús F.Ávila <36813508+JesusFAvila@users.noreply.github.com>
2022-10-31 11:23:39 +0100

    Update EntradaInputStream.java

--- src/Tema9/EntradaInputStream.java
+++ src/Tema9/EntradaInputStream.java
@@ -1,7 +1,29 @@
 package Tema9;
 
-public class InputStream {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class EntradaInputStream {
+    public static void main(String[] args) {
+        //clase InputStream
+        //FileInputStream para leer desde ficheros, se declara la variable FileInputStream e inicializo el objeto FileInputStream
+        try {
+            //Fichero con el que quiero trabajar: input.txt
+            InputStream fichero = new FileInputStream("input.txt");
+            try {
+                //Dónde quiero el fichero: variable datos
+                byte[] datos = fichero.readAllBytes();
+                //Mostrar datos mediante for:
+                for (byte dato : datos) {
+                    System.out.println((char)dato);
+                }
+            }catch (IOException e){
+                System.out.println("Imposible leer el fichero " + e.getMessage());
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("No existe el archivo seleccionado " + e.getLocalizedMessage());
+        }
     }
 }
